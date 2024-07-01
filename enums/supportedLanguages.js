8e4627a72606d14e278e516adb90f2db67b9550b
Akansha0021 <149288214+Akansha0021@users.noreply.github.com>
2024-07-01 12:02:18 +0530

    Update supportedLanguages.js

--- enums/supportedLanguages.js
+++ enums/supportedLanguages.js
@@ -1,12 +1,6 @@
 module.exports = {
-    C: 'c',
-    CPP: 'cpp',
-    PYTHON: 'python',
-    JAVA: 'java',
-    NODEJS: 'nodejs',
-    RUBY: 'ruby',
-    PROMPTV1: 'promptv1',
-    PROMPTV2: 'promptv2',
+    CPP, C, PYTHON, JAVA, NODEJS, RUBY,  PROMPTV1, PROMPTV2 
     MULTIFILE: 'multifile',
     SQLITE3: 'sqlite3',
 }
+
